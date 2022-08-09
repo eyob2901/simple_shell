ffe340e277e3eef111bafb8698c612536cbee638
Eyob Eshetu <eyob2901@gmail.com>
2022-08-09 16:21:38 -0700

    print environment

--- env.c
+++ env.c
@@ -14,22 +14,22 @@ void print_env(void)
   
 {
   
-  int x = 0;
+int x = 0;
   
-  char **env = environ;
+char **env = environ;
   
 
   
-  while (env[x])
+while (env[x])
     
-    {
+{
       
-      write(STDOUT_FILENO, (const void *)env[x], _strlen(env[x]));
+write(STDOUT_FILENO, (const void *)env[x], _strlen(env[x]));
       
-      write(STDOUT_FILENO, "\n", 1);
+write(STDOUT_FILENO, "\n", 1);
       
-      x++;
+x++;
       
-    }
+}
   
 }
